c87d1491bbde1b98c9c0feea1a2f84714adf4ef4
Kyle <kyle.mai@wiredcraft.com>
2023-09-14 15:51:51 +0800

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,7 +43,7 @@
     </section> -->
 
     <section>
-      <h2 class="text-2xl text-center text-black sm:text-4xl">合作案例</h2>
+      <h2 class="text-2xl text-center text-black sm:text-4xl">合作客户</h2>
       <el-divider class="w-1/2 mx-auto"></el-divider>
       <div class="flex flex-wrap justify-around pb-8">
         <div
@@ -54,6 +54,50 @@
           <div class="flex items-center justify-center w-32 h-32 text-white">
             <el-image class="logo" :class="c.name" :src="c.logo" fit="fill"></el-image>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <WhyUs></WhyUs>
+    </section>
+
+    <section class="px-6 py-10 sm:py-16 sm:px-24">
+      <div class="flex items-center justify-between py-6">
+        <div class="flex flex-row-reverse items-center w-0 lg:w-1/4">
+          <el-image class="block w-1/2 h-1/2" :src="logo" fit="fill"></el-image>
+        </div>
+        <div class="w-full text-left lg:w-3/4 lg:pl-36">
+          <div class="text-2xl font-bold text-black sm:text-4xl">广州领燕科技有限公司</div>
+          <div class="pt-3 leading-relaxed tracking-wide sm:pt-6">广州领燕科技有限公司是一家致力于移动互联网/创新物联网技术服务的高新技术企业，提供一站式互联网建设服务。业务包括 APP 开发、微信/支付宝/淘宝小程序开发、网站开发、系统开发及物联网研发等。自2015年以来，公司已服务 Nike、Adidas、Burberry、Starbucks、Converse、Lululemon、GOAT、山姆会员商店、宝马等多个世界知名品牌，并成功帮助客户在中国市场赢得巨大的增长。我们追求极致卓越，力求为客户打造最优质的产品，助力客户通过互联网赛道提升自身的盈利能力，实现双赢。</div>
+          <div class="pt-3 font-bold text-gray-800">地址：广州市白云区天健创意园302</div>
+        </div>
+      </div>
+
+      <el-divider></el-divider>
+
+      <div class="flex items-center justify-between pt-6">
+        <div class="flex flex-row-reverse items-center w-0 lg:w-1/4">
+          <el-image class="block w-1/2 logo-wcl h-1/2" :src="logoWCL" fit="fill"></el-image>
+        </div>
+        <div class="w-full text-left lg:w-3/4 lg:pl-36">
+          <div class="text-2xl font-bold text-black sm:text-4xl">Wiredcraft</div>
+          <div class="pt-1 leading-relaxed tracking-wide">- Wiredcraft位于上海市中心，主要对接海外客户。</div>
+          <div class="pt-3 leading-relaxed tracking-wide sm:pt-6">
+            <a class="link-primary" href="https://wiredcraft.com/" target="_blank">官方网站</a>
+          </div>
+          <div class="pt-3 font-bold text-gray-800">地址：上海市延平路121号，15D</div>
+        </div>
+      </div>
+    </section>
+
+    <section class="px-6 py-4 bg sm:py-12 sm:px-24">
+      <div class="text-xl font-bold text-primary sm:text-4xl">联系我们</div>
+      <div class="flex flex-wrap items-center justify-around pt-6 sm:pt-10">
+        <div class="flex flex-col flex-wrap items-center justify-center w-full pt-6 md:w-1/2" :class="index > 1 ? 'md:pt-16' : 'md:pt-0'" v-for="(contact, index) in contactList" :key="index">
+          <el-image class="block w-20 h-20 sm:w-24 sm:h-24" :src="contact.icon" fit="fill"></el-image>
+          <div class="text-lg md:text-xl md:pt-3">{{contact.label}}</div>
+          <div class="w-full text-xl truncate md:text-2xl md:pt-3">{{contact.content}}</div>
         </div>
       </div>
     </section>
@@ -82,6 +126,7 @@
 
 <script>
 // @ is an alias to /src
+import WhyUs from '@/components/WhyUs.vue'
 import { postList } from '@/posts/postList'
 
 export default {
@@ -91,6 +136,8 @@ export default {
       banner: require('@/assets/home-banner-bg.jpg'),
       homeCap: require('@/assets/home-cap.png'),
       logoFinal: require('@/assets/logo_final.png'),
+      logo: require('@/assets/logo_final_tb.png'),
+      logoWCL: require('@/assets/logo/wcl.svg'),
       catalogList: [
         {
           logoPosition: '',
@@ -159,10 +206,33 @@ export default {
           logo: require('@/assets/logo/bmw.png')
         }
       ],
+      contactList: [
+        {
+          icon: require('@/assets/icons/phone.png'),
+          label: '联系电话',
+          content: '150 0203 2816'
+        },
+        {
+          icon: require('@/assets/icons/wechat-dotted.png'),
+          label: '微信号',
+          content: '150 0203 2816'
+        },
+        {
+          icon: require('@/assets/icons/mail.png'),
+          label: '邮箱',
+          content: 'kyle@linkendtech.com'
+        },
+        {
+          icon: require('@/assets/icons/map.png'),
+          label: '联系地址',
+          content: '广州市白云区天健创意园302'
+        }
+      ],
       postList,
     }
   },
   components: {
+    WhyUs,
   },
   methods: {
     tapToPost(id) {
