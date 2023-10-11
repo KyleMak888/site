38291536a8a771f4406ee36d7522797b2069c1a7
Kyle <kyle.mai@wiredcraft.com>
2023-10-11 17:54:15 +0800

    update whyus copy

--- src/components/WhyUs.vue
+++ src/components/WhyUs.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="why-us" class="bg py-4 px-6 sm:py-12 sm:px-24">
-    <div class="text-primary text-xl sm:text-4xl font-bold">选择我们</div>
-    <p class="w-4/5 text-center sm:text-xl mx-auto pt-2">以客户为中心，为企业保驾护航</p>
+  <div id="why-us" class="px-6 py-4 bg sm:py-12 sm:px-24">
+    <div class="text-xl font-bold text-primary sm:text-4xl">选择我们</div>
+    <p class="w-4/5 pt-2 mx-auto text-center sm:text-xl">以客户为中心，为企业保驾护航</p>
     <div class="flex flex-wrap items-center justify-around pt-6 sm:pt-10">
       <el-card
-        class="w-64 h-80 flex flex-col overflow-visible mb-6"
+        class="flex flex-col w-64 mb-6 overflow-visible h-80"
         body-style="padding: 20px 0;"
         v-for="(flow, index) in dataList"
         :key="index"
       >
-        <div class="w-full py-3 px-2 flex items-center bg-primary rounded-r-md shadow-lg transform -translate-x-4">
-          <div class="text-5xl sm:text-6xl text-gray-100">{{index}}</div>
-          <div class="text-2xl sm:text-4xl text-left text-white flex-1 pl-3 transform translate-y-1">{{flow.label}}</div>
+        <div class="flex items-center w-full px-2 py-3 transform -translate-x-4 shadow-lg bg-primary rounded-r-md">
+          <div class="text-5xl text-gray-100 sm:text-6xl">{{index}}</div>
+          <div class="flex-1 pl-3 text-2xl text-left text-white transform translate-y-1 sm:text-4xl">{{flow.label}}</div>
         </div>
         <div class="pt-12">
           <div class="text-xl font-bold text-gray-700">{{flow.desc}}</div>
-          <div class="text-xl pt-2">{{flow.subDesc}}</div>
+          <div class="pt-2 text-xl">{{flow.subDesc}}</div>
         </div>
       </el-card>
     </div>
@@ -30,12 +30,12 @@ export default {
       dataList: [
         {
           label: '专业人员',
-          desc: '60+精英技术团队',
+          desc: '10年以上技术团队',
           subDesc: '实力雄厚'
         },
         {
           label: '经验丰富',
-          desc: '20+行业应用解决方案',
+          desc: '80+行业应用解决方案',
           subDesc: '供您选择'
         },
         {
@@ -45,7 +45,7 @@ export default {
         },
         {
           label: '工匠精神',
-          desc: '把握每一个细节',
+          desc: '把握每一个细节',
           subDesc: '精益求精'
         }
       ]
